844acb2afe245f75f5c606b06afd9e239fa9ae4d
Zvonko Biškup <zbiskup@gmail.com>
2014-03-25 23:25:26 +0100

    Making main js file

--- app/database/migrations/2014_03_03_190147_create_posts_table.php
+++ app/database/migrations/2014_03_03_190147_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration {
             $table->increments('id');
 			$table->string('title');
 			$table->text('body');
+            $table->timestamps();
         });
 	}
 
---
+++ public/js/main.js
@@ -0,0 +1,80 @@
+var titleEditor = new MediumEditor('.title-editable', {
+    buttonLabels: 'fontawesome'
+});
+var bodyEditor = new MediumEditor('.body-editable', {
+    buttonLabels: 'fontawesome'
+});
+$(function () {
+    $('.body-editable').mediumInsert({
+        editor: bodyEditor,
+        images: true,
+        imagesUploadScript: "{{ URL::to('upload') }}"
+    });
+    if ($('#hideEditor').length) {
+        $('.body-editable').mediumInsert('disable');
+        bodyEditor.deactivate();
+        titleEditor.deactivate();
+    }
+});
+$('.error').hide().empty();
+$('.success').hide().empty();
+
+$('body').on('click', '#form-submit', function(e){
+    e.preventDefault();
+    var postTitle = titleEditor.serialize();
+    var postContent = bodyEditor.serialize();
+
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url : "{{ URL::action('PostsController@store') }}",
+        data: { title: postTitle['post-title']['value'], body: postContent['post-body']['value'] },
+        success: function(data) {
+            if(data.success === false)
+            {
+                $('.error').append(data.message);
+                $('.error').show();
+            } else {
+                $('.success').append(data.message);
+                $('.success').show();
+                setTimeout(function() {
+                    window.location.href = "{{ URL::action('PostsController@index') }}";
+                }, 2000);
+            }
+        },
+        error: function(xhr, textStatus, thrownError) {
+            alert('Something went wrong. Please Try again later...');
+        }
+    });
+    return false;
+});
+
+$('body').on('click', '#form-update', function(e){
+    e.preventDefault();
+    var postTitle = titleEditor.serialize();
+    var postContent = bodyEditor.serialize();
+
+    $.ajax({
+        type: 'PUT',
+        dataType: 'json',
+        url : "{{ URL::action('PostsController@update', array(Request::segment(2))) }}",
+        data: { title: postTitle['post-title']['value'], body: postContent['post-body']['value'] },
+        success: function(data) {
+            if(data.success === false)
+            {
+                $('.error').append(data.message);
+                $('.error').show();
+            } else {
+                $('.success').append(data.message);
+                $('.success').show();
+                setTimeout(function() {
+                    window.location.href = "{{ URL::action('PostsController@index') }}";
+                }, 2000);
+            }
+        },
+        error: function(xhr, textStatus, thrownError) {
+            alert('Something went wrong. Please Try again later...');
+        }
+    });
+    return false;
+});
